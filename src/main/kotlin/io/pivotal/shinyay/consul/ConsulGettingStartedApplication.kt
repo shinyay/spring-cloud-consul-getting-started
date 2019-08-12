509ab3e22fdeb4f2d0b1037fa986d63532ea31d4
shinyay <shinya.com@gmail.com>
2019-08-12 22:32:30 +0900

    Initial Commit

---
+++ src/main/kotlin/io/pivotal/shinyay/consul/ConsulGettingStartedApplication.kt
@@ -0,0 +1,11 @@
+package io.pivotal.shinyay.consul
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConsulGettingStartedApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConsulGettingStartedApplication>(*args)
+}
