4d64759ff953eac2dc9dfd79a4f129f4acd3fa14
shinyay <shinya.com@gmail.com>
2019-08-14 21:09:45 +0900

    Add Discovery Client annotation

--- src/main/kotlin/io/pivotal/shinyay/consul/ConsulGettingStartedApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/consul/ConsulGettingStartedApplication.kt
@@ -2,8 +2,10 @@ package io.pivotal.shinyay.consul
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class ConsulGettingStartedApplication
 
 fun main(args: Array<String>) {
