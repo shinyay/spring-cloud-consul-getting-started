4990c835fbed1235a701c5eac014fabcbe9fd97c
shinyay <shinya.com@gmail.com>
2019-08-14 21:57:41 +0900

    Modify application name

--- src/main/kotlin/io/pivotal/shinyay/consul/controller/DiscoveryClientController.kt
+++ src/main/kotlin/io/pivotal/shinyay/consul/controller/DiscoveryClientController.kt
@@ -1,6 +1,8 @@
 package io.pivotal.shinyay.consul.controller
 
 import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
@@ -13,15 +15,18 @@ class DiscoveryClientController(val discoveryClient: DiscoveryClient) {
 
     @GetMapping("/discoveryClient")
     fun discoveryCheck(): String? {
-        val service: URI = discoveryClient.getInstances("myApp")
+        val service: URI = discoveryClient.getInstances("my-app")
                 .stream()
                 .findFirst()
                 .map { si -> si.uri }
-                .map { s -> s.resolve("/ping") }
+                .map { s -> s.resolve("/hello") }
                 .get()
         return restTemplate.getForEntity(service, String::class.java).body
     }
 
     @GetMapping("/hello")
     fun hello():String = "Hello"
+
+    @GetMapping("/my-health-check")
+    fun healthCheck(): ResponseEntity<URI> = ResponseEntity(HttpStatus.OK)
 }
