117d3633b13f6a4f00c16d7d1899bce954682a1b
shinyay <shinya.com@gmail.com>
2019-08-14 21:17:24 +0900

    Modify to optimize import

--- src/main/kotlin/io/pivotal/shinyay/consul/controller/DiscoveryClientController.kt
+++ src/main/kotlin/io/pivotal/shinyay/consul/controller/DiscoveryClientController.kt
@@ -1,6 +1,5 @@
 package io.pivotal.shinyay.consul.controller
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
